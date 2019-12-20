e9f363be7b962d651622a5ef7c0ec3894e50cbf0
Pedro Ruiz <ruizhidalgopedro@gmail.com>
2019-12-20 11:55:28 +0100

    renamed for each test according its functions name

--- Validator.test.js
+++ Validator.test.js
@@ -2,59 +2,59 @@ import Validator from "./Validator"
 
 describe('Testing validator', () => {
   
-  test('should object', () => {
+  test('testing type()', () => {
     expect(Validator({}).type()).toEqual('object')
   })
 
-  test('should string', () => {
+  test('tesint isString()', () => {
     expect(Validator('string').isString()).toEqual(true)
   })
 
-  test('should number', () => {
+  test('testing isNumber()', () => {
     expect(Validator(3.14).isNumber()).toEqual(true)
   })
 
-  test('should object', () => {
+  test('testing isObject()', () => {
     expect(Validator({}).isObject()).toEqual(true)
   })
 
-  test('should isBoolean', () => {
+  test('testing isBoolean', () => {
     expect(Validator(false).isBoolean()).toEqual(true)
   })
 
-  test('should eu date', () => {
+  test('testing \'eu\' isDate()', () => {
     expect(Validator('15/12/2019').isDate('eu')).toEqual(true)
   })
 
-  test('should us date', () => {
+  test('testing \'us\' isDate()', () => {
     expect(Validator('12/15/2019').isDate('us')).toEqual(true)
   })
 
-  test('should is email', () => {
+  test('testing isEmail()', () => {
     expect(Validator('e@ma.il').isEmail('eu')).toEqual(true)
   })
 
-  test('should length', () => {
+  test('testing length()', () => {
     expect(Validator('abc').length()).toEqual(3)
   })
 
-  test('should lengthMoreThan', () => {
+  test('testing lengthMoreThan()', () => {
     expect(Validator('abc').lengthMoreThan(2)).toEqual(true)
   })
 
-  test('should lengthLessThan', () => {
+  test('testing lengthLessThan()', () => {
     expect(Validator('abc').lengthLessThan(5)).toEqual(true)
   })
 
-  test('should lengthEqualTo', () => {
+  test('testing lengthEqualTo()', () => {
     expect(Validator('abc').lengthEqualTo(5)).toEqual(false)
   })
   
-  test('should passwordMatch', () => {
+  test('testing passwordMatch()', () => {
     expect(Validator('abc').passwordMatch('qaz')).toEqual(false)
   })
 
-  test('should regExp', () => {
+  test('testing regExp()', () => {
     expect(Validator('HelloWorld').regExp(/^[a-z0-9]+$/i)).toEqual(true)
   })
 
